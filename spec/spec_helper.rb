10bda8b2373804004ddde58b25f0c91f47a9ad07
scarvill91 <scarvill91@gmail.com>
2015-07-12 15:17:31 -0500

    Extract error messages to constants in shared context for use in specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,11 @@ RSpec.shared_context "default_values" do
   BOARD_SIZE = 3
 end
 
+RSpec.shared_context "error_messages" do
+  OUT_OF_BOUNDS_ERROR_MSG = 'Cell coordinates are out of bounds'
+  NON_EMPTY_CELL_ERROR_MSG = 'Cannot alter marked cell'
+end
+
 RSpec.shared_context "helper_methods" do
   def random_coordinate(board_size)
     [rand(board_size), rand(board_size)]
