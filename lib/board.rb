4dd56d090cfb28fb6d9f06eca6930aa8c7ebf400
scarvill91 <scarvill91@gmail.com>
2015-07-11 22:28:37 -0500

    Implement Board initialization and generation of game board

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,23 @@
 module TicTacToe
   class Board
+    attr_reader :size
+
+    def initialize(perameters)
+      @size = perameters[:size]
+      generate_new_board
+    end
+
+    def num_cells
+      @cells.flatten.count
+    end
+
+    private
+
+    def generate_new_board
+      @cells = []
+      @size.times do
+        @cells << (1..@size).map{nil}
+      end
+    end
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,4 +1,23 @@
 require 'spec_helper'
 
 describe TicTacToe::Board do
+
+  describe '#initialize' do
+
+    it 'takes a perameters hash' do
+      perameters = {size: 3}
+
+      expect{TicTacToe::Board.new(perameters)}.not_to raise_error
+    end
+
+    it 'generates a NxN board of the given size' do
+      (1..3).each do |size|
+        perameters = {size: size}
+        board = TicTacToe::Board.new(perameters)
+
+        expect(board.size).to eq size
+        expect(board.num_cells).to eq size**2
+      end
+    end
+  end
 end
