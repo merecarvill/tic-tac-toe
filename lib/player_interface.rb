41f6bcf48d4105097915d7212a311476d77e3442
scarvill91 <scarvill91@gmail.com>
2015-07-12 16:10:13 -0500

    Implement PlayerInterface.required_methods - returns names of all required methods in symbol form

--- lib/player_interface.rb
+++ lib/player_interface.rb
@@ -2,6 +2,10 @@ module TicTacToe
   class PlayerInterface
     InterfaceError = Class.new(StandardError)
 
+    def self.required_methods
+      [:move]
+    end
+
     def move
       raise InterfaceError, "#{self.class}#move is not implemented"
     end
--- spec/player_interface_spec.rb
+++ spec/player_interface_spec.rb
@@ -3,6 +3,15 @@ require 'spec_helper'
 describe TicTacToe::PlayerInterface do
   let(:interface_error) {TicTacToe::PlayerInterface::InterfaceError}
 
+  describe '.requred_methods' do
+
+    it 'provides the name of each method that must be implemented' do
+      [:move].each do |method|
+        expect(described_class.required_methods.include?(method)).to be true
+      end
+    end
+  end
+
   describe '#move' do
 
     it 'raises error if not implemented by subclass' do
