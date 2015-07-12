a596e729859484c21f9af26c3f3ee208be192317
scarvill91 <scarvill91@gmail.com>
2015-07-11 23:28:28 -0500

    Replace mentions of TicTacToe::Board in spec with described_class

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,13 +7,13 @@ describe TicTacToe::Board do
     let(:default_perameters) { {size: BOARD_SIZE} }
 
     it 'takes a perameters hash' do
-      expect{TicTacToe::Board.new(default_perameters)}.not_to raise_error
+      expect{described_class.new(default_perameters)}.not_to raise_error
     end
 
     it 'generates a NxN board of the given size' do
       (1..3).each do |size|
         perameters = {size: size}
-        board = TicTacToe::Board.new(perameters)
+        board = described_class.new(perameters)
 
         expect(board.size).to eq size
         expect(board.num_cells).to eq size**2
