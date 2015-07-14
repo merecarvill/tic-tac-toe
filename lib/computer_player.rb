084a735ff61d6a6b22cbe505114d465e0c717b24
scarvill91 <scarvill91@gmail.com>
2015-07-13 23:15:55 -0500

    Implement ComputerPlayer#minimax_score
    Scores game states for purposes of minimax algorithm. Currently a very simple
    heuristic, where win = 1, loss = -1, and draw = 0

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -1,15 +1,23 @@
 require 'player_interface'
+require 'game_state'
 
 module TicTacToe
   class ComputerPlayer < PlayerInterface
-    attr_reader :mark
 
-    def initialize(perameters)
-      @mark = perameters[:mark]
-      @board = perameters[:board]
+    def initialize(parameters)
+      @player_mark = parameters[:player]
+      @opponent_mark = parameters[:opponent]
+      @board = parameters[:board]
     end
 
-    def move
+    def minimax_score(game_state)
+      game_state.board.lines.each do |line|
+        return 1 if line[:cells].all?{ |cell| cell == @player_mark }
+        return -1 if line[:cells].all?{ |cell| cell == @opponent_mark }
+      end
+
+      return 0 if game_state.board.filled?
+      return nil
     end
   end
 end
--- spec/computer_player_spec.rb
+++ spec/computer_player_spec.rb
@@ -1,24 +1,126 @@
 require 'spec_helper'
 
 describe TicTacToe::ComputerPlayer do
-  shared_context "default_values"
+  include_context "helper_methods"
 
   let(:blank_board) { TicTacToe::Board.new({size: BOARD_SIZE}) }
-  let(:ai_as_x) { described_class.new({mark: :x, board: blank_board}) }
 
   it 'implements PlayerInterface' do
+    player_mark, opponent_mark = PLAYER_MARKS.shuffle
+    ai = described_class.new(board: blank_board, player: player_mark, opponent: opponent_mark)
+
     TicTacToe::PlayerInterface.required_methods.each do |method|
-      expect(ai_as_x).to respond_to(method)
+      expect(ai).to respond_to(method)
     end
   end
 
   describe '#initialize' do
 
-    it 'takes a perameters hash with the player\'s mark and a reference to the game board' do
-      mark = PLAYER_MARKS.shuffle
-      ai = described_class.new({mark: mark, board: blank_board})
+    it 'takes a parameters hash with the player\'s mark, opponent\'s mark, and the game board' do
+      player_mark, opponent_mark = PLAYER_MARKS.shuffle
+      params = {board: blank_board, player: player_mark, opponent: opponent_mark}
 
-      expect(ai.mark).to eq mark
+      expect{described_class.new(params)}.not_to raise_error
+    end
+  end
+
+  describe '#minimax_score' do
+
+    it 'returns 1 when given game state is a win for computer player' do
+      player_mark, opponent_mark = PLAYER_MARKS.shuffle
+      ai = described_class.new(board: blank_board, player: player_mark, opponent: opponent_mark)
+
+      # horizontal wins
+      (0...BOARD_SIZE).each do |row|
+        board = blank_board.deep_copy
+        (0...BOARD_SIZE).each{ |col| board[row, col] = player_mark }
+        game_state = TicTacToe::GameState.new(board: board, player: player_mark, opponent: opponent_mark)
+
+        expect(ai.minimax_score(game_state)).to eq 1
+      end
+
+      # vertical wins
+      (0...BOARD_SIZE).each do |col|
+        board = blank_board.deep_copy
+        (0...BOARD_SIZE).each{ |row| board[row, col] = player_mark }
+        game_state = TicTacToe::GameState.new(board: board, player: player_mark, opponent: opponent_mark)
+
+        expect(ai.minimax_score(game_state)).to eq 1
+      end
+
+      # diagonal wins
+      ldiag_board = blank_board.deep_copy
+      (0...BOARD_SIZE).each{ |index| ldiag_board[index, index] = player_mark }
+      ldiag_game_state = TicTacToe::GameState.new(board: ldiag_board, player: player_mark, opponent: opponent_mark)
+
+      rdiag_board = blank_board.deep_copy
+      (0...BOARD_SIZE).each{ |index| rdiag_board[index, rdiag_board.size - index - 1] = player_mark }
+      rdiag_game_state = TicTacToe::GameState.new(board: rdiag_board, player: player_mark, opponent: opponent_mark)
+
+      expect(ai.minimax_score(ldiag_game_state)).to eq 1
+      expect(ai.minimax_score(rdiag_game_state)).to eq 1
+    end
+
+    it 'returns -1 when given game state is a win for opponent' do
+      player_mark, opponent_mark = PLAYER_MARKS.shuffle
+      ai = described_class.new(board: blank_board, player: player_mark, opponent: opponent_mark)
+
+      # horizontal wins
+      (0...BOARD_SIZE).each do |row|
+        board = blank_board.deep_copy
+        (0...BOARD_SIZE).each{ |col| board[row, col] = opponent_mark }
+        game_state = TicTacToe::GameState.new(board: board, player: player_mark, opponent: opponent_mark)
+
+        expect(ai.minimax_score(game_state)).to eq -1
+      end
+
+      # vertical wins
+      (0...BOARD_SIZE).each do |col|
+        board = blank_board.deep_copy
+        (0...BOARD_SIZE).each{ |row| board[row, col] = opponent_mark }
+        game_state = TicTacToe::GameState.new(board: board, player: player_mark, opponent: opponent_mark)
+
+        expect(ai.minimax_score(game_state)).to eq -1
+      end
+
+      # diagonal wins
+      ldiag_board = blank_board.deep_copy
+      (0...BOARD_SIZE).each{ |index| ldiag_board[index, index] = opponent_mark }
+      ldiag_game_state = TicTacToe::GameState.new(board: ldiag_board, player: player_mark, opponent: opponent_mark)
+
+      rdiag_board = blank_board.deep_copy
+      (0...BOARD_SIZE).each{ |index| rdiag_board[index, rdiag_board.size - index - 1] = opponent_mark }
+      rdiag_game_state = TicTacToe::GameState.new(board: rdiag_board, player: player_mark, opponent: opponent_mark)
+
+      expect(ai.minimax_score(ldiag_game_state)).to eq -1
+      expect(ai.minimax_score(rdiag_game_state)).to eq -1
+    end
+
+    it 'returns 0 if game state is a draw' do
+      player_mark, opponent_mark = PLAYER_MARKS.shuffle
+      ai = described_class.new(board: blank_board, player: player_mark, opponent: opponent_mark)
+
+      board = blank_board.deep_copy
+      (0...BOARD_SIZE).each do |row|
+        (0...BOARD_SIZE).each do |col|
+          if row == 0
+            board[row, col] = col.even? ? player_mark : opponent_mark
+          else
+            board[row, col] = col.odd? ? player_mark : opponent_mark
+          end
+        end
+      end
+      game_state = TicTacToe::GameState.new(board: board, player: player_mark, opponent: opponent_mark)
+
+      expect(ai.minimax_score(game_state)).to eq 0
+    end
+
+    it 'returns nil if game state is unfinished' do
+      player_mark, opponent_mark = PLAYER_MARKS.shuffle
+      ai = described_class.new(board: blank_board, player: player_mark, opponent: opponent_mark)
+      game_state = TicTacToe::GameState.new(board: blank_board, player: player_mark, opponent: opponent_mark)
+
+      expect(ai.minimax_score(game_state)).to eq nil
     end
   end
 end
