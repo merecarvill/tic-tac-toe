9c0a7f9f475c4f41ad0791bf9911340f89d74d65
scarvill91 <scarvill91@gmail.com>
2015-07-13 20:25:57 -0500

    Implement GameState#make_move
    Generates a new game state, with a new board marked with player's mark at given
    coordinates, and which remembers the move that generated the game state

--- lib/game_state.rb
+++ lib/game_state.rb
@@ -1,10 +1,23 @@
 module TicTacToe
   class GameState
+    attr_reader :board, :last_move
 
     def initialize(parameters)
       @board = parameters[:board]
-      @player = parameters[:player]
-      @opponent = parameters[:opponent]
+      @player_mark = parameters[:player]
+      @opponent_mark = parameters[:opponent]
+      @last_move = parameters[:last_move]
+    end
+
+    def make_move(coordinate)
+      new_board = @board.deep_copy
+      new_board[*coordinate] = @player_mark
+      GameState.new(
+        board: new_board,
+        player: @player_mark,
+        opponent: @opponent_mark,
+        last_move: coordinate
+      )
     end
   end
 end
--- spec/game_state_spec.rb
+++ spec/game_state_spec.rb
@@ -1,15 +1,62 @@
 require 'spec_helper'
 
 describe TicTacToe::GameState do
+  include_context "helper_methods"
+
+  let(:board) { TicTacToe::Board.new({size: BOARD_SIZE, other_board: nil}) }
 
   describe '#initialize' do
 
     it 'takes a parameters hash with a game board, current player\'s mark, and opponent\'s mark' do
-      board = TicTacToe::Board.new({size: BOARD_SIZE, other_board: nil})
       player_mark, opponent_mark = PLAYER_MARKS.shuffle
       params = {board: board, player: player_mark, opponent: opponent_mark}
 
       expect{described_class.new(params)}.not_to raise_error
+    end
+  end
+
+  describe '#make_move' do
+
+    context 'when board is blank at given coordinate' do
+
+      it 'returns a new game state object' do
+        player_mark, opponent_mark = PLAYER_MARKS.shuffle
+        params = {board: board, player: player_mark, opponent: opponent_mark}
+        initial_game_state = described_class.new(params)
+        new_game_state = initial_game_state.make_move(random_coordinate(board.size))
+
+        expect(new_game_state).not_to eq initial_game_state
+      end
+
+      it 'creates a new board object to associate with the new game state' do
+        player_mark, opponent_mark = PLAYER_MARKS.shuffle
+        params = {board: board, player: player_mark, opponent: opponent_mark}
+        initial_game_state = described_class.new(params)
+        new_game_state = initial_game_state.make_move(random_coordinate(board.size))
+
+        expect(new_game_state.board).not_to eq initial_game_state.board
+      end
+
+      it 'marks the new game state board with player\'s mark at given coordinate' do
+        player_mark, opponent_mark = PLAYER_MARKS.shuffle
+        params = {board: board, player: player_mark, opponent: opponent_mark}
+        initial_game_state = described_class.new(params)
+        coordinate = random_coordinate(board.size)
+        new_game_state = initial_game_state.make_move(coordinate)
+
+        expect(new_game_state.board[*coordinate]).to eq player_mark
+      end
+
+      it 'remembers the last move made in the new game state' do
+        player_mark, opponent_mark = PLAYER_MARKS.shuffle
+        params = {board: board, player: player_mark, opponent: opponent_mark}
+        initial_game_state = described_class.new(params)
+        coordinate = random_coordinate(board.size)
+        new_game_state = initial_game_state.make_move(coordinate)
+
+        expect(new_game_state.last_move).to eq coordinate
+        expect(initial_game_state.last_move).not_to eq coordinate
+      end
     end
   end
 end
