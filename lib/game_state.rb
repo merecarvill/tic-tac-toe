ffcb17497794c3633aa033785327701a26af632e
scarvill91 <scarvill91@gmail.com>
2015-07-13 19:10:27 -0500

    Implement GameState#Initialize

--- lib/game_state.rb
+++ lib/game_state.rb
@@ -1,5 +1,10 @@
 module TicTacToe
   class GameState
 
+    def initialize(parameters)
+      @board = parameters[:board]
+      @player = parameters[:player]
+      @opponent = parameters[:opponent]
+    end
   end
 end
--- spec/game_state_spec.rb
+++ spec/game_state_spec.rb
@@ -2,4 +2,14 @@ require 'spec_helper'
 
 describe TicTacToe::GameState do
 
+  describe '#initialize' do
+
+    it 'takes a parameters hash with a game board, current player\'s mark, and opponent\'s mark' do
+      board = TicTacToe::Board.new({size: BOARD_SIZE, other_board: nil})
+      player_mark, opponent_mark = PLAYER_MARKS.shuffle
+      params = {board: board, player: player_mark, opponent: opponent_mark}
+
+      expect{described_class.new(params)}.not_to raise_error
+    end
+  end
 end
