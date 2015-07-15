c2d73078b6f0d8a85f3a6dca089f9d0cded035dd
scarvill91 <scarvill91@gmail.com>
2015-07-14 20:53:07 -0500

    Pull common setup out of ComputerPlayer#minimax_score tests and into helper methods

--- spec/computer_player_spec.rb
+++ spec/computer_player_spec.rb
@@ -28,72 +28,40 @@ describe TicTacToe::ComputerPlayer do
 
     it 'returns 1 when given game state is a win for computer player' do
       player_mark, opponent_mark = PLAYER_MARKS.shuffle
-      ai = described_class.new(board: blank_board, player: player_mark, opponent: opponent_mark)
+      ai = described_class.new(
+        board: new_board(BOARD_SIZE),
+        player: player_mark,
+        opponent: opponent_mark
+      )
 
-      # horizontal wins
-      (0...BOARD_SIZE).each do |row|
-        board = blank_board.deep_copy
-        (0...BOARD_SIZE).each{ |col| board[row, col] = player_mark }
-        game_state = TicTacToe::GameState.new(board: board, player: player_mark, opponent: opponent_mark)
-
-        expect(ai.minimax_score(game_state)).to eq 1
-      end
-
-      # vertical wins
-      (0...BOARD_SIZE).each do |col|
-        board = blank_board.deep_copy
-        (0...BOARD_SIZE).each{ |row| board[row, col] = player_mark }
-        game_state = TicTacToe::GameState.new(board: board, player: player_mark, opponent: opponent_mark)
+      all_wins(BOARD_SIZE, player_mark).each do |winning_board|
+        game_state = TicTacToe::GameState.new(
+          board: winning_board,
+          player: player_mark,
+          opponent: opponent_mark
+        )
 
         expect(ai.minimax_score(game_state)).to eq 1
       end
-
-      # diagonal wins
-      ldiag_board = blank_board.deep_copy
-      (0...BOARD_SIZE).each{ |index| ldiag_board[index, index] = player_mark }
-      ldiag_game_state = TicTacToe::GameState.new(board: ldiag_board, player: player_mark, opponent: opponent_mark)
-
-      rdiag_board = blank_board.deep_copy
-      (0...BOARD_SIZE).each{ |index| rdiag_board[index, rdiag_board.size - index - 1] = player_mark }
-      rdiag_game_state = TicTacToe::GameState.new(board: rdiag_board, player: player_mark, opponent: opponent_mark)
-
-      expect(ai.minimax_score(ldiag_game_state)).to eq 1
-      expect(ai.minimax_score(rdiag_game_state)).to eq 1
     end
 
     it 'returns -1 when given game state is a win for opponent' do
       player_mark, opponent_mark = PLAYER_MARKS.shuffle
-      ai = described_class.new(board: blank_board, player: player_mark, opponent: opponent_mark)
+      ai = described_class.new(
+        board: new_board(BOARD_SIZE),
+        player: player_mark,
+        opponent: opponent_mark
+      )
 
-      # horizontal wins
-      (0...BOARD_SIZE).each do |row|
-        board = blank_board.deep_copy
-        (0...BOARD_SIZE).each{ |col| board[row, col] = opponent_mark }
-        game_state = TicTacToe::GameState.new(board: board, player: player_mark, opponent: opponent_mark)
-
-        expect(ai.minimax_score(game_state)).to eq -1
-      end
-
-      # vertical wins
-      (0...BOARD_SIZE).each do |col|
-        board = blank_board.deep_copy
-        (0...BOARD_SIZE).each{ |row| board[row, col] = opponent_mark }
-        game_state = TicTacToe::GameState.new(board: board, player: player_mark, opponent: opponent_mark)
+      all_wins(BOARD_SIZE, opponent_mark).each do |winning_board|
+        game_state = TicTacToe::GameState.new(
+          board: winning_board,
+          player: player_mark,
+          opponent: opponent_mark
+        )
 
         expect(ai.minimax_score(game_state)).to eq -1
       end
-
-      # diagonal wins
-      ldiag_board = blank_board.deep_copy
-      (0...BOARD_SIZE).each{ |index| ldiag_board[index, index] = opponent_mark }
-      ldiag_game_state = TicTacToe::GameState.new(board: ldiag_board, player: player_mark, opponent: opponent_mark)
-
-      rdiag_board = blank_board.deep_copy
-      (0...BOARD_SIZE).each{ |index| rdiag_board[index, rdiag_board.size - index - 1] = opponent_mark }
-      rdiag_game_state = TicTacToe::GameState.new(board: rdiag_board, player: player_mark, opponent: opponent_mark)
-
-      expect(ai.minimax_score(ldiag_game_state)).to eq -1
-      expect(ai.minimax_score(rdiag_game_state)).to eq -1
     end
 
     it 'returns 0 if game state is a draw' do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,4 +23,41 @@ RSpec.shared_context "helper_methods" do
   def all_coordinates(board_size)
     (0...board_size).to_a.repeated_permutation(2).to_a
   end
+
+  def new_board(board_size)
+    TicTacToe::Board.new({size: board_size})
+  end
+
+  def all_wins(board_size, winning_mark)
+    winning_boards = []
+    winning_boards.concat horizontal_wins(board_size, winning_mark)
+    winning_boards.concat vertical_wins(board_size, winning_mark)
+    winning_boards.concat diagonal_wins(board_size, winning_mark)
+  end
+
+  def horizontal_wins(board_size, winning_mark)
+    (0...board_size).each_with_object([]) do |row, winning_boards|
+      board = new_board(board_size)
+      (0...board_size).each{ |col| board[row, col] = winning_mark }
+      winning_boards << board
+    end
+  end
+
+  def vertical_wins(board_size, winning_mark)
+    (0...board_size).each_with_object([]) do |col, winning_boards|
+      board = new_board(board_size)
+      (0...board_size).each{ |row| board[row, col] = winning_mark }
+      winning_boards << board
+    end
+  end
+
+  def diagonal_wins(board_size, winning_mark)
+    ldiag_board = new_board(board_size)
+    (0...board_size).each{ |index| ldiag_board[index, index] = winning_mark }
+
+    rdiag_board = new_board(board_size)
+    (0...board_size).each{ |index| rdiag_board[index, rdiag_board.size - index - 1] = winning_mark }
+
+    [ldiag_board, rdiag_board]
+  end
 end
