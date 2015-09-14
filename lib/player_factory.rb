0c1e7fdf644c3bbffab69723dfea3c2863104513
Spencer <scarvill91@gmail.com>
2015-09-14 16:18:42 -0500

    Change PlayerFactory from module to class
    Also partition player creation into private methods

--- lib/player_factory.rb
+++ lib/player_factory.rb
@@ -2,23 +2,33 @@ require_relative 'human_player'
 require_relative 'computer_player'
 
 module TicTacToe
-  module PlayerFactory
+  class PlayerFactory
     def self.build(config)
-      shared_parameters = {player_mark: config[:player_mark]}
-
       case config[:type]
       when :human
-        human_parameters = {
-          interface: config[:game].interface
-        }
-        HumanPlayer.new(human_parameters.merge(shared_parameters))
+        create_human_player(config)
       when :computer
-        computer_parameters = {
-          opponent_mark: (config[:game].player_marks - [config[:player_mark]]).pop,
-          board: config[:game].board
-        }
-        ComputerPlayer.new(computer_parameters.merge(shared_parameters))
+        create_computer_player(config)
       end
+    end
+
+    private
+
+    def self.create_human_player(config)
+      human_parameters = {
+        player_mark: config[:player_mark],
+        interface: config[:game].interface
+      }
+      HumanPlayer.new(human_parameters)
+    end
+
+    def self.create_computer_player(config)
+      computer_parameters = {
+        player_mark: config[:player_mark],
+        opponent_mark: (config[:game].player_marks - [config[:player_mark]]).pop,
+        board: config[:game].board
+      }
+      ComputerPlayer.new(computer_parameters)
     end
   end
 end
