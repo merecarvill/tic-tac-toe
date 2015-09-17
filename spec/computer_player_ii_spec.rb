b1758dd7bbc274e1573f445a7ee36574f8b44c38
Spencer <scarvill91@gmail.com>
2015-09-17 09:16:03 -0500

    Refactor new ComputerPlayer tests

--- spec/computer_player_ii_spec.rb
+++ spec/computer_player_ii_spec.rb
@@ -5,28 +5,32 @@ module TicTacToe
     include_context "default_values"
     include_context "helper_methods"
 
+    let(:_) { Board.blank_mark }
+    let(:x) { @default_first_player }
+    let(:o) { @default_second_player }
+
     def new_computer_player(parameters)
       ComputerPlayerII.new(parameters)
     end
 
-    let(:_) { Board.blank_mark }
-    let(:x) { @default_first_player }
-    let(:o) { @default_second_player }
+    def first_player(board)
+      parameters = {
+        board: board,
+        player_mark: x,
+        opponent_mark: o
+      }
+      new_computer_player(parameters)
+    end
 
     describe "#move" do
       it "returns the coordinates of a valid move" do
         board_config = [
           x, o, x,
-          o, x, o,
-          x, o, _
+          x, x, o,
+          o, o, _
         ]
-        board = build_board(board_config.shuffle)
-        parameters = {
-          board: board,
-          player_mark: x,
-          opponent_mark: o
-        }
-        computer_player = new_computer_player(parameters)
+        board = build_board(board_config)
+        computer_player = first_player(board)
         coordinates = computer_player.move
 
         expect(board.out_of_bounds?(coordinates)).to be false
@@ -36,12 +40,7 @@ module TicTacToe
       context "when game board has a center space and it is blank" do
         it "returns the center coordinates" do
           board_size = @default_board_size.odd? ? @default_board_size : 3
-          parameters = {
-            board: blank_board(board_size),
-            player_mark: x,
-            opponent_mark: o
-          }
-          computer_player = new_computer_player(parameters)
+          computer_player = first_player(blank_board(board_size))
 
           expect(computer_player.move).to eq [board_size / 2, board_size / 2]
         end
